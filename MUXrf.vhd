3c88c71a12339b62d9f03ea42fb96246bf8782e9
FedericoMolinaChavez <federicomolinachavez@gmail.com>
2016-03-08 20:51:38 -0500

    Added files via upload

---
+++ MUXrf.vhd
@@ -0,0 +1,11 @@
+entity MUXrf is
+	port(
+	ent1: in STD_LOGIC_VECTOR (4 downto 0);
+	ent2: in STD_LOGIC_VECTOR (4 downto 0);
+	sal_ out STD_LOGIC_VECTOR (4 downto 0)
+	);
+end MUXrf;
+
+architecture MUXrf_arch of MUXrf is
+
+end MUXrf_arch;
