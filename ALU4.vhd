5e8bd9fa37ef67cda35a9fcd846ddacd00fe859b
Juan David Ospina <danju9593@hotmail.com>
2016-03-08 21:09:41 -0500

    ALU Suma 4
    Alu que suma a la direccion del pc 4

---
+++ ALU4.vhd
@@ -0,0 +1,18 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+
+entity ALU4 is 
+	port (	
+			addressALU4: in std_logic_vector(31 downto 0);
+			resultado: out std_logic_vector
+			);
+end ALU4;
+
+architecture alu4_arch of ALU4 is
+begin
+	process(addressALU4)
+	begin
+		resultado <= ((addressALU4)+4);
+	end process;
+end alu4_arch;
