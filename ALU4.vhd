0d835dbf7566cf4f47b9340a264cc28f2f34ebd4
Juan David Ospina <danju9593@hotmail.com>
2016-03-08 21:10:41 -0500

    Update ALU4.vhd
    arreglo del ancho de bits de resultado. de 1 bit a 32 bits

--- ALU4.vhd
+++ ALU4.vhd
@@ -5,7 +5,7 @@ use IEEE.std_logic_unsigned.all;
 entity ALU4 is 
 	port (	
 			addressALU4: in std_logic_vector(31 downto 0);
-			resultado: out std_logic_vector
+			resultado: out std_logic_vector(31 downto 0)
 			);
 end ALU4;
 
